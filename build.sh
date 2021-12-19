095dd7f48fa7fc45acb9d32bce131132036bc5a6
Sushrut1101 <guptasushrut@gmail.com>
2021-12-19 20:19:38 +0530

    Recovery-Builder: Modify the fox_11.0 Builder
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Change to the Home Directory
+cd ~
+
+# Clone the Sync Repo
+git clone $FOX_SYNC
+cd sync
+
+# Sync the Sources
+./orangefox_sync.sh --branch $FOX_BRANCH --path $SYNC_PATH
+
+# Change to the Source Directory
+cd $SYNC_PATH
+
+# Clone Trees
+git clone $DT_LINK $DT_PATH
+
+# ccache
+if [-z "$CCACHE_SIZE"]; then
+    ccache -M 10G
+else
+    ccache -M ${CCACHE_SIZE}
+fi
+
+# Run the Extra Command
+$EXTRA_CMD
+
+# Prepare the Build Environment
+source build/envsetup.sh
+
+# lunch the target
+if ["$FOX_BRANCH" = "fox_11.0"]; then
+    lunch twrp_${DEVICE}-eng
+else
+    lunch omni_${DEVICE}-eng
+fi
+
+# Build the Code
+if [-z "$J_VAL"]; then
+    mka -j$(nproc --all) $TARGET
+elif ["$J_VAL"="0"]; then
+    mka $TARGET
+else
+    mka -j${J_VAL} $TARGET
+fi
+
+# Exit
+exit 0
