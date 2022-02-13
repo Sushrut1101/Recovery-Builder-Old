a145c00ee8f7c70658cfd958f2deea7c9e5a2263
Sushrut1101 <guptasushrut@gmail.com>
2022-02-13 18:37:26 +0530

    Enhanced Support for PRs
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ vars.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Device
+export FOX_BRANCH="fox_9.0"
+export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+
+export DEVICE="beryllium"
+export OEM="xiaomi"
+export TARGET="recoveryimage"
+
+export DT_PATH="device/$OEM/$DEVICE"
+export OUTPUT="OrangeFox*.zip"
+
+# Extra Command
+export EXTRA_CMD="git clone https://github.com/OrangeFoxRecovery/Avatar.git misc"
+
+# Not Recommended to Change
+export SYNC_PATH="$HOME/work" # Full (absolute) path. Use "$HOME" instead of "~".
+export USE_CCACHE=1
+export CCACHE_SIZE="50G"
+export CCACHE_DIR="$HOME/work/.ccache"
+export J_VAL=16
