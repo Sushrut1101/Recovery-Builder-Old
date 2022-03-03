ce90d52cad622321ca5cd7ab4308fa3028afdc1e
Sushrut1101 <guptasushrut@gmail.com>
2022-03-03 18:31:24 +0530

    Do not Allow 'curl' in vars.sh
    Co-authored-by: Apon77 <khalakuzzamanapon5@gmail.com>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- checks.sh
+++ checks.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-if [ ! -z $(cat vars.sh 2>&1 | grep "TG") ]; then
-    echo "ERROR - You Cannot Set *TG* Vars in your vars.sh"
+CONFIG="vars.sh"
+
+if [ ! -z $(cat $CONFIG 2>&1 | grep "TG") ]; then
+    echo "ERROR - You Cannot Set *TG* Vars in your $CONFIG"
+    exit 1
+fi
+
+# Do not allow curl
+curl_check=$(grep 'curl ' $CONFIG | wc -l)
+if [[ $curl_check -gt 0 ]]; then
+    echo -e "Please dont use \'curl\' in $CONFIG".
     exit 1
 fi
