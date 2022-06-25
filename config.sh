07319a07c1d376fa7fd1f8bf201cc4cac03ce01c
Jeick18 <85885611+Jeick18@users.noreply.github.com>
2022-06-24 22:28:41 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://github.com/brigudav/android_device_xiaomi_surya_twrp -b android-12.1"
 
-export DEVICE="beryllium"
+export DEVICE="surya"
 export OEM="xiaomi"
 
 # Build Target
